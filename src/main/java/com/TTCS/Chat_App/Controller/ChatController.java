27ac8926ec949d222a03cc5df1017a7e586db80c
NguyenAnhTruong2K5 <nguyenanhtruong2k5@gmail.com>
2026-03-29 15:01:56 +0700

    Add lombok plugin for maven

--- src/main/java/com/TTCS/Chat_App/Controller/ChatController.java
+++ src/main/java/com/TTCS/Chat_App/Controller/ChatController.java
@@ -13,9 +13,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
